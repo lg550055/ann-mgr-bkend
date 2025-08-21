3e7a2e2f2431b64727ee4fa0a1ceb66e5233d11e
lg550055 <lg550055@hotmail.com>
2025-08-21 14:21:21 -0700

    Corrects db url

--- api/database.py
+++ api/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 import os
 
 load_dotenv()
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
+DATABASE_URL = os.getenv("DB_URL", "sqlite:///./test.db")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
